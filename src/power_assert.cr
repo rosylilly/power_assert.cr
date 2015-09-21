150bc75ea24327f91dab5eef83fc27f637b64321
Sho Kusano <rosylilly@aduca.org>
2015-09-22 05:57:30 +0900

    v0.2.1

--- src/power_assert.cr
+++ src/power_assert.cr
@@ -1,7 +1,7 @@
 require "spec"
 
 module PowerAssert
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 
   Operators = [
     "!", "!=", "%", "&", "*", "**", "+", "-", "/", "<", "<<", "<=", "<=>", "==", "===",
